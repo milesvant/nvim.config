6522cb8e0972c591842c83a0b3b62da8c9c0ea67
Miles Van Tongeren <milesvant@gmail.com>
2023-06-23 10:00:18 -0700

    Add split pane remaps

--- lua/milesvant/remap.lua
+++ lua/milesvant/remap.lua
@@ -1,2 +1,8 @@
 vim.g.mapleader = ","
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Split pane navigation shortcuts
+vim.api.nvim_set_keymap("n", "<C-h>", "<C-w><C-h>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-j>", "<C-w><C-j>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-k>", "<C-w><C-k>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-l>", "<C-w><C-l>", { noremap = true, silent = true })
