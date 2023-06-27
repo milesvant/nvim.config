995ebc55ff681b9a0d889942400e4a20e5054bc6
Miles Van Tongeren <milesvant@gmail.com>
2023-06-26 22:59:27 -0700

    Add copilot setup script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+git clone https://github.com/github/copilot.vim.git \
+  ~/.config/nvim/pack/github/start/copilot.vim
