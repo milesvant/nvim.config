c70db09d1b432e1b33e24b1031aa728057ae600f
Miles Van Tongeren <milesvant@gmail.com>
2023-08-14 14:29:44 -0700

    Add node to setup.sh and suppress git clone errors

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# Node is required for Copilot.
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    brew list -q node || brew install node
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo apt install -y node
+fi
+
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim 2> /dev/null
 
 git clone https://github.com/github/copilot.vim.git \
-  ~/.config/nvim/pack/github/start/copilot.vim
+  ~/.config/nvim/pack/github/start/copilot.vim 2> /dev/null
