5c2ce124dc609f2ea1fe48459b5bc281cb428e22
Kerry Lu <kerryleonlu@yahoo.com>
2023-09-15 20:58:16 -0700

    Add README and add installing neovim to setup

--- setup.sh
+++ setup.sh
@@ -7,8 +7,20 @@ elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
     sudo apt install -y nodejs
 fi
 
+# neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
+
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+nvim
+
+# packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim 2> /dev/null
 
+# copilot
 git clone https://github.com/github/copilot.vim.git \
   ~/.config/nvim/pack/github/start/copilot.vim 2> /dev/null
