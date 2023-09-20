f7191bc642f5a8e5c8e443e073bf26c7c3e67219
Kerry Lu <kerryleonlu@yahoo.com>
2023-09-19 21:48:35 -0700

    Move vi alias to setup.sh

--- setup.sh
+++ setup.sh
@@ -16,6 +16,8 @@ chmod u+x nvim.appimage
 sudo mv squashfs-root /
 sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 
+echo 'alias vi=nvim' >> ~/.bashrc 
+
 # packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim 2> /dev/null
