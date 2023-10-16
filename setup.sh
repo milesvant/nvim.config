a114e367e219682718dd224d63f55be41348c8f9
Kerry Lu <kerryleonlu@yahoo.com>
2023-10-15 22:36:20 -0700

    Use xclip clipboard for yank/delete/change and paste without overwriting register

--- setup.sh
+++ setup.sh
@@ -18,6 +18,10 @@ sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 
 echo 'alias vi=nvim' >> ~/.bashrc 
 
+# xclip
+sudo apt-get update
+sudo apt-get install xclip
+
 # packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim 2> /dev/null
