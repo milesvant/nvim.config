c2e04294bde428a3d01b34b806483e29d3a43209
Kerry Lu <141074773+kerry-vorticity@users.noreply.github.com>
2024-01-17 15:57:20 -0800

    rm node (#27)

--- setup.sh
+++ setup.sh
@@ -6,20 +6,6 @@ elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
    echo "Setting up for Linux..."
 fi
 
-# Node is required for Copilot.
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    brew list -q node || brew install node
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    sudo apt-get update
-    sudo apt-get install -y ca-certificates curl gnupg
-    sudo mkdir -p /etc/apt/keyrings
-    curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-    NODE_MAJOR=20
-    echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-    sudo apt-get update
-    sudo apt-get install nodejs -y
-fi
-
 # neovim
 if [[ "$OSTYPE" == "darwin"* ]]; then
     brew install neovim
