e0ccfb8a77f590d29b6f45808dc046965b014b08
Miles Van Tongeren <milesvant@gmail.com>
2024-08-19 12:58:19 -0700

    Add rust-tools

--- lua/milesvant/packer.lua
+++ lua/milesvant/packer.lua
@@ -5,41 +5,44 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
 
--- Packer can manage itself
-use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.4',
-  requires = { {'nvim-lua/plenary.nvim'} }
-}
+    use {
+      'nvim-telescope/telescope.nvim', tag = '0.1.4',
+      requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-use {
-  'nvim-lualine/lualine.nvim',
-  requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-}
+    use {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
 
-use { "folke/tokyonight.nvim" }
+    use { "folke/tokyonight.nvim" }
 
-use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-}
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
 
-use "nvim-tree/nvim-web-devicons"
-use {
-    "cbochs/grapple.nvim",
-    requires = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
-}
+    use "nvim-tree/nvim-web-devicons"
+    use {
+        "cbochs/grapple.nvim",
+        requires = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
+    }
 
-use {
-    'numToStr/Comment.nvim',
-    config = function()
-        require('Comment').setup()
-    end
-}
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
+    }
 
-use 'williamboman/mason.nvim'    
-use 'williamboman/mason-lspconfig.nvim'
+    use 'williamboman/mason.nvim'
+    use 'williamboman/mason-lspconfig.nvim'
+
+    use 'neovim/nvim-lspconfig'
+    use 'simrat39/rust-tools.nvim'
 
 end)
 
